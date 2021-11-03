85ddd897aba7749efc33836034b450f78dcea476
Kozue Yamada <kozue.yamada@iths.se>
2021-11-03 15:37:26 +0100

    Constructor will throw IllegalArgumentException

--- src/main/java/com/gameoflife/Game.java
+++ src/main/java/com/gameoflife/Game.java
@@ -1,4 +1,18 @@
 package com.gameoflife;
 
 public class Game {
+
+    private int width;
+    private int height;
+    private int[][] board;
+
+    public Game(int width, int height) {
+        if (width < 0 || height < 0) {
+            throw new IllegalArgumentException("Numbers should be natural");
+        }
+        this.width = width;
+        this.height = height;
+        this.board = new int[width][height];
+    }
+
 }
--- src/test/java/com/gameoflife/GameTest.java
+++ src/test/java/com/gameoflife/GameTest.java
@@ -1,7 +1,17 @@
 package com.gameoflife;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
+
+    @Test
+    void constructorWillThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Game testSimulation1 = new Game(-1, 5);
+            Game testSimulation2 = new Game(5, -3);
+        });
+    }
 
 }
